fa5a14a1e61dcc7d5c2e1a66e0abf093cc55e59c
Mohamed_Ali_Shaltoot <mohamedali3092002@gmail.com>
2025-07-27 19:53:54 +0300

    addtask func done

--- lib/to_do_using_firebase/features/home_screen/manager/provider/home_provider.dart
+++ lib/to_do_using_firebase/features/home_screen/manager/provider/home_provider.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:xo_game/core/models/task_model.dart';
+import 'package:xo_game/core/service/fire_base_functions.dart';
 import 'package:xo_game/to_do_using_firebase/features/settings_screen/settings_screen.dart';
 import 'package:xo_game/to_do_using_firebase/features/tasks_screen/task_screen.dart';
 
@@ -7,6 +9,8 @@ class HomeProvider extends ChangeNotifier {
   DateTime selectedDate = DateTime.now();
   DateTime selectedDatePicker = DateTime.now();
   List<Widget> screens = [const TaskScreen(), const SettingsScreen()];
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
   void setIndex(int index) {
     selectedIndex = index;
     notifyListeners();
@@ -20,5 +24,14 @@ class HomeProvider extends ChangeNotifier {
   void setDatePicker(DateTime date) {
     selectedDatePicker = date;
     notifyListeners();
+  }
+
+  void addTask() {
+    TaskModel taskModel = TaskModel(
+        title: titleController.text,
+        description: descriptionController.text,
+        time: selectedDatePicker.millisecondsSinceEpoch,
+        isDone: false);
+    FireBaseFunctions.addTask(taskModel);
   }
 }
